3aadad76507721433dbf5b340a448ec464e7ac7e
Katya <ekaterinadurocher@gmail.com>
2022-10-11 12:25:53 -0400

    php first try

--- form project/myform.php
+++ form project/myform.php
@@ -1,3 +1,23 @@
-<form action="appform.html" method="post">
+<?php
+       $name = $_POST['name'];
+       $email = $_POST['email'];
+       $phone = $_POST['phone'];
+       $visit_location = $_POST['visit-location'];
+       $visit_date = $_POST['visit-date'];
+       $app_type = $_POST['app-type'];
+       $satisfaction = $_POST['Satisfaction']
+       $comments = $_POST['comments'];
+       $from = 'From: My Contact Form';
+       $to = 'ekaterinadurocher@gmail.com';
+       $subject = 'New Form FeedBack!';
 
-</form>
+       $body = "From: $name\n E-Mail: $email\n Message:\n $comments\n $phone\n $visit_location\n $visit_date\n $app_type\n $satisfaction";
+
+       if ($_POST['submit']) {
+           if (mail ($to, $subject, $body, $from)) {
+           echo '<p>Message Sent Successfully!</p>';
+           } else {
+           echo '<p>Ah! Try again, please?</p>';
+           }
+       }
+    ?>
